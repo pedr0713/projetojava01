52aa476953a460b051015295fb403301715d9155
Pedro <Pedro@pedro.com>
2023-03-06 19:43:14 -0300

    Revert "Adicionando coisas"
    This reverts commit b9bc2c55fb993878125dbeaad52169896e63f2f2.

--- projetojava1/src/projetojava1/teste/ClassePrimeira.java
+++ projetojava1/src/projetojava1/teste/ClassePrimeira.java
@@ -2,9 +2,6 @@ package projetojava1.teste;
 
 public class ClassePrimeira {
 	public static void main(String[] args) {
-		System.out.println(1+3); //pooo
-		
-		int n2 = 2;
-		int n3 = 3;
+		System.out.println(1+3);
 	}
 }
